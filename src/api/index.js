2cbb59c138e84e841b03f7effa8f1ab49467db8c
Santosh Singh <santosh.singh@gmail.com>
2017-01-13 18:18:31 +0000

    Adding updated code

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,12 @@
 import { version } from '../../package.json';
 import { Router } from 'express';
 import facets from './facets';
+import low from 'lowdb';
+
+var db = low('/home/ubuntu/workspace/db.json')
+db.defaults({employees:[]}).value();
+
+var users= db.get('employees').find({}).value();
 
 export default ({ config, db }) => {
 	let api = Router();
@@ -8,6 +14,20 @@ export default ({ config, db }) => {
 	// mount the facets resource
 	api.use('/facets', facets({ config, db }));
 
+api.get('/services/search/whoami', (req, res) => {
+	console.log( "request for authentication")
+	res.json({result:"Success"});
+});
+
+api.get('/api/v1/Person/basicchanges', (req, res) => {
+	console.log("Hello")
+	    let hello ="Hello World"
+	    let ts=req.params.ts;
+	    console.log("request for person data")
+	    let sendjson = {response:{docs:null}};
+	    sendjson.response.docs=users;
+		res.json( sendjson);
+	});
 	// perhaps expose some API metadata at the root
 	api.get('/', (req, res) => {
 		res.json({ version });
--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+'use strict'
+
 import http from 'http';
 import express from 'express';
 import cors from 'cors';
@@ -6,10 +8,34 @@ import initializeDb from './db';
 import middleware from './middleware';
 import api from './api';
 import config from './config.json';
+import basicAuth from 'basic-auth';
+
+var auth = function (req, res, next) {
+  function unauthorized(res) {
+    res.set('WWW-Authenticate', 'Basic realm=Authorization Required');
+    return res.send(401);
+  };
+
+  var user = basicAuth(req);
+
+  if (!user || !user.name || !user.pass) {
+    return unauthorized(res);
+  };
+
+  if (user.name === 'foo' && user.pass === 'foobar') {
+    return next();
+  } else {
+    return unauthorized(res);
+  };
+};
 
 let app = express();
-app.server = http.createServer(app);
+let apiai = require('apiai');
+let apiaiapp = apiai("71ced92360244263978c1130878483fd");
+ 
 
+app.server = http.createServer(app);
+ 
 // 3rd party middleware
 app.use(cors({
 	exposedHeaders: config.corsHeaders
@@ -25,10 +51,27 @@ initializeDb( db => {
 	// internal middleware
 	app.use(middleware({ config, db }));
 
-	// api router
-	app.use('/api', api({ config, db }));
-
-	app.server.listen(process.env.PORT || config.port);
+	// myblackrock router
+	app.use('/myblackrock',auth, api({ config, db }));
+	app.get('/myblackrock/:query', function (req, res) {
+	  var request = apiaiapp.textRequest(req.params.query, {
+    sessionId: '<asdfasdfasdfasdfunique session id>'
+    });
+    
+   
+    request.on('response', function(response) {
+        console.log(response);
+        res.json(response.result.parameters);
+    });
+     
+    request.on('error', function(error) {
+        console.log(error);
+        res.json(error);
+    }); 
+    request.end();
+  })
+	console.log( "port is " + process.env.PORT);
+	app.server.listen(process.env.PORT);
 
 	console.log(`Started on port ${app.server.address().port}`);
 });
